986017435c82e16261a914985d30037eb74df36d
cpappas-code <cpappas@forestareaschools.org>
2025-10-29 10:07:37 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'times-table-turbo-v4'; // Increment version if you change assets
+const CACHE_NAME = 'times-table-turbo-v3'; // Increment version if you change assets
 // Define the files to cache
 const urlsToCache = [
     '.', // This caches the index.html at the root
@@ -51,4 +51,5 @@ self.addEventListener('activate', event => {
         })
     );
 });
+
 
